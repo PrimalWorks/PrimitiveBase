4667ae509705665080f58fab21c7706615986e46
David Ritchie <dave.ritchie@veracityuk.com>
2018-10-14 00:50:58 +0100

    Adding BaseNotifiable

---
+++ src/PBase/BaseNotifiable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PBase
+{
+    public abstract class BaseNotifiable : BaseDisposable, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Set<T>(ref T field, T value, [CallerMemberName] string name = "")
+        {
+            if (
+                ((field == null) && (value == null)) ||
+                ((field != null) && field.Equals(value))
+                )
+            {
+                return;
+            }
+
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
