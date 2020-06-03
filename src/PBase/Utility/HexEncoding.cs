89a3a5146a2a764f2ba9305d00c89c54aa6583ea
Adam Stanton <adam.stanton@veracityglobal.com>
2020-06-03 15:23:55 +0100

    Document exceptions + Add arguments for hex string format

--- src/PBase/Utility/HexEncoding.cs
+++ src/PBase/Utility/HexEncoding.cs
@@ -7,29 +7,49 @@ namespace PBase.Utility
     {
         /// <summary>Converts a text string into a hex encoded format.</summary>
         /// <param name="stringToHex">The string to be hex encoded.</param>
+        /// <param name="removeDashes">Remove dashes from the hex encoded string</param>
+        /// <param name="toLowercase">Set the characters of the hex encoded string to lower case</param>
         /// <returns>The hex encoded string.</returns>
-        public static string ToHex(this string stringToHex)
+        /// <exception cref="FormatException"> Thrown when a null or empty stringToHex is provided</exception>
+        public static string ToHex(this string stringToHex, bool removeDashes = true, bool toLowercase = true)
         {
             if (string.IsNullOrEmpty(stringToHex))
             {
-                throw new System.ArgumentException("String to hex cannot be null or empty");
+                throw new System.FormatException("String to hex cannot be null or empty");
             }
 
             var bytes = Encoding.Default.GetBytes(stringToHex);
             var hexString = BitConverter.ToString(bytes);
-            hexString = hexString.Replace("-", "");
-            hexString = hexString.ToLower();
+
+            if(removeDashes)
+            {
+                hexString = hexString.Replace("-", "");
+            }
+
+            if (toLowercase)
+            {
+                hexString = hexString.ToLower();
+            }
+
             return hexString;
         }
 
         /// <summary>Decodes a hex encoded string.</summary>
         /// <param name="hexString">The hex encoded string to decode.</param>
         /// <returns>The decoded hex string.</returns>
+        /// <exception cref="FormatException"> Thrown when a null, empty, or invalid hexString is provided</exception>
         public static string FromHex(this string hexString)
         {
             if (string.IsNullOrEmpty(hexString))
             {
-                throw new System.ArgumentException("Hex string cannot be null or empty");
+                throw new System.FormatException("Hex string cannot be null or empty");
+            }
+
+            hexString = hexString.Replace("-", "");
+
+            if (hexString.Length % 2 != 0)
+            {
+                throw new System.FormatException("Hex string length cannot be an odd number");
             }
 
             var bytes = new byte[hexString.Length / 2];
