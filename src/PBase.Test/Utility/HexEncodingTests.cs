f3d07bbfbb0417e2a1e79cdfcc0de5a3e922aed8
Adam Stanton <adam.stanton@veracityglobal.com>
2020-06-03 10:47:57 +0100

    Add hex encoding/decoding utility for strings

---
+++ src/PBase.Test/Utility/HexEncodingTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using PBase.Utility;
+using Xunit;
+
+namespace PBase.Test.Utility
+{
+
+    public class TestHexEncodingTests
+    {
+
+        private static class TestConstants
+        {
+            public const string UnencodedText = "A string of text that can be encoded to and decoded from hex!";
+            public const string HexEncodedText = "4120737472696e67206f66207465787420746861742063616e20626520656e636f64656420746f20616e64206465636f6465642066726f6d2068657821";
+        }
+
+        [Fact]
+        public void TestHexEncoding()
+        {
+            const string stringToHex = TestConstants.UnencodedText;
+            var encodedString = stringToHex.ToHex();
+            Assert.Equal(TestConstants.HexEncodedText, encodedString);
+        }
+
+        [Fact]
+        public void TestHexDecoding()
+        {
+            const string hexString = TestConstants.HexEncodedText;
+            var decodedString = hexString.FromHex();
+            Assert.Equal(TestConstants.UnencodedText, decodedString);
+        }
+
+        [Fact]
+        public void TestHexEncodeDecodeThrowsOnEmptyString()
+        {
+            const string emptyString = "";
+            Assert.Throws<ArgumentException>(() => { emptyString.ToHex();});
+            Assert.Throws<ArgumentException>(() => { emptyString.FromHex();});
+        }
+
+        [Fact]
+        public void TestHexEncodeDecodeThrowsOnNullString()
+        {
+            const string nullString = null;
+            Assert.Throws<ArgumentException>(() => { nullString.ToHex();});
+            Assert.Throws<ArgumentException>(() => { nullString.FromHex();});
+        }
+
+        [Fact]
+        public void TestHexDecodeWithInvalidHexString()
+        {
+            const string hexString = "This Is An Invalid Hex String";
+            Assert.Throws<FormatException>(() => { hexString.FromHex();});
+        }
+    }
+}
---
+++ src/PBase/Utility/HexEncoding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace PBase.Utility
+{
+    public static class HexEncoding
+    {
+        /// <summary>Converts a text string into a hex encoded format.</summary>
+        /// <param name="stringToHex">The string to be hex encoded.</param>
+        /// <returns>The hex encoded string.</returns>
+        public static string ToHex(this string stringToHex)
+        {
+            if (string.IsNullOrEmpty(stringToHex))
+            {
+                throw new System.ArgumentException("String to hex cannot be null or empty");
+            }
+
+            var bytes = Encoding.Default.GetBytes(stringToHex);
+            var hexString = BitConverter.ToString(bytes);
+            hexString = hexString.Replace("-", "");
+            hexString = hexString.ToLower();
+            return hexString;
+        }
+
+        /// <summary>Decodes a hex encoded string.</summary>
+        /// <param name="hexString">The hex encoded string to decode.</param>
+        /// <returns>The decoded hex string.</returns>
+        public static string FromHex(this string hexString)
+        {
+            if (string.IsNullOrEmpty(hexString))
+            {
+                throw new System.ArgumentException("Hex string cannot be null or empty");
+            }
+
+            var bytes = new byte[hexString.Length / 2];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2), 16);
+            }
+            return Encoding.Default.GetString(bytes);
+        }
+    }
+}
